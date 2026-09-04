671f34e6aff8bee7166e1184244a4e5d85976e66
Jose Antonio Balderrama Hernández <L26550881@chihuahua2.tecnm.mx>
2026-09-03 18:45:10 -0600

    Add files via upload

---
+++ EVA1_3_SALIDAS/src/eva1_3_salidas/EVA1_3_SALIDAS.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_salidas;
+
+/**
+ *
+ * @author Josea
+ */
+public class EVA1_3_SALIDAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a Fundamwntos de Programacion.");
+        System.out.println("Jose Antonio Balderrama Hernandez.");
+        System.out.print("Tu calificacion es:");
+        System.out.println(85);
+    }
+           
+}
