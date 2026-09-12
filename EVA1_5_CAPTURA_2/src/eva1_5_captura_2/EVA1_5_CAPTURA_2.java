230591ec2c15ccdb74768405c7461dfd32f9cc49
Jose Antonio Balderrama Hernández <L26550881@chihuahua2.tecnm.mx>
2026-09-11 23:35:16 -0600

    Add files via upload

---
+++ EVA1_5_CAPTURA_2/src/eva1_5_captura_2/EVA1_5_CAPTURA_2.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_captura_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_5_CAPTURA_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //DECLARAMOS LAS VARIABLES
+        String nombre;
+        int edad;
+        double salario;
+        //SCANNER
+        Scanner captu = new Scanner(System.in);
+        //CAPTURA
+        System.out.println("Captura el nombre del usuario: ");
+        nombre = captu.nextLine();//lee texto hasta el "ENTER"
+        System.out.println("Captura la edad:");
+        edad = captu.nextInt();
+        System.out.println("Captura el salario:");
+        salario = captu.nextDouble();
+        System.out.println("DATOS CAPTURADOS:");
+        System.out.print("Nombre: ");
+        System.out.println(nombre);
+        System.out.print("Edad: ");
+        System.out.println(edad);
+        System.out.print("Salario: ");
+        System.out.println(salario);
+    }
+    
+}
