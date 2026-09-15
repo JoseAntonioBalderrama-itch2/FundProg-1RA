68f13dd87c7b23f0b81b2656c759c457be87186f
Jose Antonio Balderrama Hernández <L26550881@chihuahua2.tecnm.mx>
2026-09-14 22:54:06 -0600

    Add files via upload

---
+++ EVA1_9_OPERACIONES_CADENAS/src/eva1_9_operaciones_cadenas/EVA1_9_OPERACIONES_CADENAS.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_operaciones_cadenas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_9_OPERACIONES_CADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //TAMBIEN HAY OPERACIONES PARA CADENAS
+        //CONCATENACION: PEGAR CADENAS DE TEXTO --> +
+        //cuando un operador hace diferentes acciones se le llama SOBRECARGA
+        //NO HAY MENOS EN LAS CADENAS
+        String cade;
+        cade = "Hola" + " " + "mundo";
+        System.out.println(cade);
+        cade = "Valor = " + 100;
+        System.out.println(cade);
+        cade = "Valores booleanos = " + false;
+        System.out.println(cade);
+        
+    }
+    
+}
